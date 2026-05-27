21d1b497c7c231d677866bd10e9f4208a4c7d5b2
Mahammadali12 <zamanli.mehemmedeli@gmail.com>
2026-05-27 16:00:51 -0400

    Fix

--- check_service.sh
+++ check_service.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
 	echo "Usage: check_service.sh <service_name>"
+	exit 2
 fi
 
 service=$(systemctl show -p ActiveState "$1" | awk -F= '{print $2}')
 
+if ! systemctl cat "$1" &>/dev/null; then
+	echo -e "\e[1;31mUNKNOWN\e[0m: $1 does not exist"
+    exit 3
+fi
+
 if [[ $service == "inactive" ]]; then
 	echo -e "\e[1;31mCRITICAL\e[0m: $1 is not running"
+	exit 1
 elif [[ $service == "active" ]]; then
 	echo -e "\e[1;32mOK\e[0m: $1 is running"
+	exit 0
 fi
--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-while true; do
 	
 	usage_per=$(df / | awk ' NR==2 {sub(/%/,"",$5) ;print $5}')
 
-	if [[ $usage_per -gt 1 ]]; then
+	if [[ $usage_per -gt 80 ]]; then
 		echo "WARNING $(date +"%Y-%m-%d %H:%M:%S") - Disk usage at $usage_per%" >> /tmp/monitor.log
 	fi
 
@@ -16,4 +15,3 @@ while true; do
 	fi
 	
 	sleep 30
-done
